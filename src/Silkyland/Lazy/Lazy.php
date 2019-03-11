109fab8b433b4b51ba019da650cd7a1e293ba0f7
Bundit Nuntates <silkyland@gmail.com>
2019-03-11 11:51:51 +0700

    add dev branch

---
+++ src/Silkyland/Lazy/Lazy.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace Silkyland\Lazy;
+
+use Intervention\Image\File;
+use Intervention\Image\Image;
+use Carbon\Carbon;
+
+/**
+ * Class Lazy
+ * @package Silkyland\Lazy
+ */
+class Lazy
+{
+    /**
+     * @var
+     */
+    public $file;
+    /**
+     * @var int
+     */
+    public $baseWidth = 350;
+    /**
+     * @var null
+     */
+    public $baseHeight = null;
+    /**
+     * @var int
+     */
+    public $width = 250;
+    /**
+     * @var int
+     */
+    public $height = 250;
+    /**
+     * @var string
+     */
+    private $savePath;
+    /**
+     * @var
+     */
+    private $extension;
+    /**
+     * @var
+     */
+    private $randomNumber;
+    /**
+     * @var
+     */
+    public $filename;
+    /**
+     * @var
+     */
+    private $filePath;
+    /**
+     * @var
+     */
+    private $uploadedFile;
+    /**
+     * @var
+     */
+    private $fullPath;
+
+    /**
+     * ImageUpload constructor.
+     * @param $file
+     * @param string $savePath
+     */
+    public function __construct($file, $savePath = "/images")
+    {
+        $this->file = $file;
+        $this->savePath = $savePath;
+        $this->filePath = public_path($this->savePath);
+        if (!File::exists($this->filePath)) {
+            File::makeDirectory($this->filePath);
+        }
+
+        $this->generateNewName();
+    }
+
+    /**
+     *
+     */
+    private function getExtension()
+    {
+        $this->extension = $this->file->getClientOriginalExtension();
+    }
+
+    /**
+     *
+     */
+    private function getRandomNumber()
+    {
+        $this->randomNumber = sha1(Carbon::now() . microtime());
+    }
+
+    /**
+     *
+     */
+    private function generateNewName()
+    {
+        $this->getExtension();
+        $this->getRandomNumber();
+        $this->fullPath = $this->filePath . '/' . $this->randomNumber . '.' . $this->extension;
+        $this->filename = $this->savePath . '/' . $this->randomNumber . '.' . $this->extension;
+    }
+
+    /**
+     *
+     */
+    public function upload()
+    {
+        $this->uploadedFile = Image::make($this->file);
+    }
+
+    /**
+     * @param string $option
+     */
+    public function resize($option = '')
+    {
+        $this->uploadedFile->resize($this->baseWidth, $this->baseHeight, function ($constraint) use ($option) {
+            if ($option === 'aspect') {
+                $constraint->aspectRatio();
+            }
+        });
+    }
+
+    /**
+     *
+     */
+    public function crop()
+    {
+        $this->uploadedFile->crop($this->width, $this->height);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function save()
+    {
+        $this->uploadedFile->save($this->fullPath);
+        return $this->filename;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function execute()
+    {
+        $this->upload();
+        $this->resize();
+        $this->crop();
+        return $this->save();
+    }
+}
---
+++ tests/LazyTest.php
@@ -0,0 +1,8 @@
+<?php
+
+use Silkyland\Lazy\Lazy;
+
+class LazyTest extends PHPUnit_Framework_TestCase
+{
+
+}
